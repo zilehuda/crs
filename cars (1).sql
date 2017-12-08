57954de0e63ad29cbf626fda4df0642a8e31a531
zilehuda <zilehuda.tariq@gmail.com>
2017-12-08 21:26:05 +0500

    db

--- cars (1).sql
+++ cars (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 05, 2017 at 09:37 PM
+-- Generation Time: Dec 08, 2017 at 05:25 PM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 5.6.30
 
@@ -29,17 +29,17 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `admin` (
-  `admin_id` int(11) NOT NULL,
-  `uname` varchar(255) NOT NULL,
-  `pass` varchar(255) NOT NULL
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `admin`
 --
 
-INSERT INTO `admin` (`admin_id`, `uname`, `pass`) VALUES
-(1, 'admin', 'admin');
+INSERT INTO `admin` (`id`, `email`, `password`) VALUES
+(2, 'admin', '$2y$10$dT1hQ5wSB4vHP1vbh0SX/O4AA3duWd1Bj/D1cyCdFnZP/KHEkoCzm');
 
 -- --------------------------------------------------------
 
@@ -96,22 +96,27 @@ CREATE TABLE `cars` (
   `expiry_year` text NOT NULL,
   `status` varchar(255) NOT NULL,
   `seller_id` int(11) NOT NULL,
-  `manu_year` text NOT NULL
+  `manu_year` text NOT NULL,
+  `image` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `cars`
 --
 
-INSERT INTO `cars` (`car_id`, `car_name`, `company`, `fuel_type`, `capacity`, `ins_no`, `hire_cost`, `mileage`, `air_bag`, `gps`, `reg_no`, `expiry_month`, `expiry_year`, `status`, `seller_id`, `manu_year`) VALUES
-(1, 'Mercedes Benz', '', '', '5', '', 20000, '0', '0', '0', '', '', '', 'Available', 0, '0'),
-(2, 'Range Rover', '', '', '6', '', 30000, '0', '0', '0', '', '', '', 'Available', 0, '0'),
-(3, 'Harrier', '', '', '6', '', 20000, '0', '0', '0', '', '', '', 'Available', 0, '0'),
-(5, 'LandCruiser V8', '', '', '5', '', 20000, '0', '0', '0', '', '', '', 'Available', 0, '0'),
-(6, 'Security Vehicles', '', '', '8', '', 30000, '0', '0', '0', '', '', '', 'Available', 0, '0'),
-(7, 'Wedding Limousine', '', '', '10', '', 50000, '0', '0', '0', '', '', '', 'Available', 0, '0'),
-(8, 'kk', '22', 'gas', 'k', 'kkk', 33, '33', 'yes', 'yes', '3222', 'jan', '2005', 'no', 3, '22lkll'),
-(9, 'k', '1', 'gas', 'kk', 'kk', 22, '222', 'yes', 'yes', '33', 'jan', '2005', 'no', 3, 'kk');
+INSERT INTO `cars` (`car_id`, `car_name`, `company`, `fuel_type`, `capacity`, `ins_no`, `hire_cost`, `mileage`, `air_bag`, `gps`, `reg_no`, `expiry_month`, `expiry_year`, `status`, `seller_id`, `manu_year`, `image`) VALUES
+(1, 'Mercedes Benz', '', '', '5', '', 20000, '0', '0', '0', '', '', '', 'Available', 0, '0', 'uploads/1512748242.jpg'),
+(2, 'Range Rover', '', '', '6', '', 30000, '0', '0', '0', '', '', '', 'Available', 0, '0', NULL),
+(3, 'Harrier', '', '', '6', '', 20000, '0', '0', '0', '', '', '', 'Available', 0, '0', NULL),
+(5, 'LandCruiser V8', '', '', '5', '', 20000, '0', '0', '0', '', '', '', 'Available', 0, '0', NULL),
+(6, 'Security Vehicles', '', '', '8', '', 30000, '0', '0', '0', '', '', '', 'Available', 0, '0', NULL),
+(7, 'Wedding Limousine', '', '', '10', '', 50000, '0', '0', '0', '', '', '', 'Available', 0, '0', NULL),
+(8, 'kk', '22', 'gas', 'k', 'kkk', 33, '33', 'yes', 'yes', '3222', 'jan', '2005', 'available', 3, '22lkll', NULL),
+(9, 'k', '1', 'gas', 'kk', 'kk', 22, '222', 'yes', 'yes', '33', 'jan', '2005', 'no', 3, 'kk', NULL),
+(10, 'mehran', 'suzuki', 'cng', '1234', '22222', 12345, '234', 'yes', 'yes', '3333', 'jan', '2005', 'available', 3, '2014', NULL),
+(11, 'j', 'trej', 'gas', 'j', 'jj', 333, '0000', 'yes', 'yes', '09999', 'jan', '2005', 'available', 3, 'j', '1512749153.jpg'),
+(12, 'k', 'kkk', 'gas', 'k', 'kk', 0, '00', 'yes', 'yes', '0', 'jan', '2005', 'available', 3, 'kk', 'uploads/1512749603.jpg'),
+(13, 'k', 'kkk', 'gas', 'k', 'kk', 0, '00', 'yes', 'yes', '0', 'jan', '2005', 'available', 3, 'kk', 'uploads/1512749603.jpg');
 
 -- --------------------------------------------------------
 
@@ -140,7 +145,7 @@ INSERT INTO `car_bookin` (`client_id`, `car_id`, `status`, `payment`, `hire_date
 (10, 11, 'Approved', 'paid', NULL, NULL),
 (10, 12, 'Approved', 'paid', NULL, NULL),
 (10, 13, 'Pending', 'not', NULL, NULL),
-(15, 1, 'approved', 'not', '2017-12-07', '2017-12-07');
+(15, 2, 'approved', 'not', '2017-12-08', '2017-12-16');
 
 -- --------------------------------------------------------
 
@@ -164,7 +169,7 @@ CREATE TABLE `client` (
   `fname` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` text NOT NULL,
-  `phone` int(11) NOT NULL,
+  `phone` text NOT NULL,
   `city` varchar(255) NOT NULL,
   `gender` varchar(255) NOT NULL,
   `nic` text NOT NULL,
@@ -176,20 +181,20 @@ CREATE TABLE `client` (
 --
 
 INSERT INTO `client` (`id`, `fname`, `email`, `password`, `phone`, `city`, `gender`, `nic`, `remember_token`) VALUES
-(2, 'felix kiamba', 'kiambafelix@yahoo.com', '30073147', 705053484, 'nairobi', 'Male', '', NULL),
-(3, 'concepter', 'concybogita@gmail.com', '27695131', 707403614, 'kisii', 'Female', '', NULL),
-(4, 'enock bosire', 'enock@gmail.com', '1234567', 717056766, 'narok', 'Male', '', NULL),
-(5, 'pawan', 'k142173@nu.edu.pk', '22', 0, 'karachi', 'Male', '', NULL),
-(6, 'kumar', 'khatri.catten22@gmail.com', '12345', 2147483647, 'karachi', 'Male', '', NULL),
-(7, 'pawan', 'pawan@pawan.com', '12', 2147483647, 'karachi', 'Male', '', NULL),
-(8, 'nothing', 'nothing@nu.edu.pk', '12', 0, 'aads', 'Male', '', NULL),
-(9, 'karan', 'karan@nu.edu.pk', '12', 2147483647, 'abtabad', 'Male', '', NULL),
-(10, 'buyer1', 'buyer@nu.edu.pk', '12', 2147483647, 'karachi', 'Male', '', NULL),
-(11, 'ff', 'ddd', 'ddd', 1234, 'fdfd', 'male', 'dd', NULL),
-(13, 'shayan', 'shayan@shayan.com', '$2y$10$DjcjnztbQqQqtaNXKKcIjutGtJFtQQAlXtNyvvPGcBEz0crIfQoSa', 3434, 'karachi', 'Male', '123456', NULL),
-(14, 'taha', 'taha@taha.com', '$2y$10$y.q1fzVkwJf.x4z/vCM7m.HtJSBB23adoK3IkPuO32IMLHe/PfXTC', 1235, 'karahci', 'Male', '124', NULL),
-(15, 'zilehuda', 'zilehuda@gmail.com', '$2y$10$mmQeA8fKtHnS4IwEgOM6r.Y6MqImtsQAmH.wmYef6M7GGdvApaZX.', 12345, 'karachi', 'male', '12345', 'Ed2Yo5PXuzkzg2nMTCpuA1cjgjRlQIGItZC8JHN3PPJYNnYUbxMblYMggnvg'),
-(16, 'ando', 'ando@ando.com', '$2y$10$eGF3DULVpObu4M8tsm3I0ub0mag4uuvMWhKe6/x.bfQXd8TR0Con6', 123456, 'karachi', 'Female', '123456', NULL);
+(2, 'felix kiamba', 'kiambafelix@yahoo.com', '30073147', '705053484', 'nairobi', 'Male', '', NULL),
+(3, 'concepter', 'concybogita@gmail.com', '27695131', '707403614', 'kisii', 'Female', '', NULL),
+(4, 'enock bosire', 'enock@gmail.com', '1234567', '717056766', 'narok', 'Male', '', NULL),
+(5, 'pawan', 'k142173@nu.edu.pk', '22', '0', 'karachi', 'Male', '', NULL),
+(6, 'kumar', 'khatri.catten22@gmail.com', '12345', '2147483647', 'karachi', 'Male', '', NULL),
+(7, 'pawan', 'pawan@pawan.com', '12', '2147483647', 'karachi', 'Male', '', NULL),
+(8, 'nothing', 'nothing@nu.edu.pk', '12', '0', 'aads', 'Male', '', NULL),
+(9, 'karan', 'karan@nu.edu.pk', '12', '2147483647', 'abtabad', 'Male', '', NULL),
+(10, 'buyer1', 'buyer@nu.edu.pk', '12', '2147483647', 'karachi', 'Male', '', NULL),
+(11, 'ff', 'ddd', 'ddd', '1234', 'fdfd', 'male', 'dd', NULL),
+(13, 'shayan', 'shayan@shayan.com', '$2y$10$DjcjnztbQqQqtaNXKKcIjutGtJFtQQAlXtNyvvPGcBEz0crIfQoSa', '3434', 'karachi', 'Male', '123456', NULL),
+(14, 'taha', 'taha@taha.com', '$2y$10$y.q1fzVkwJf.x4z/vCM7m.HtJSBB23adoK3IkPuO32IMLHe/PfXTC', '1235', 'karahci', 'Male', '124', NULL),
+(15, 'zilehuda', 'zilehuda@gmail.com', '$2y$10$mmQeA8fKtHnS4IwEgOM6r.Y6MqImtsQAmH.wmYef6M7GGdvApaZX.', '12345', 'karachi', 'male', '12345', 'qTGDyJiHMGqzxz6Uf0XTteT5TcHyBKNTHTWoscdWOtC9IOpjht6z1ojpG9gD'),
+(22, 'umer', 'umer@umer.com', '$2y$10$dVkfm96.0jfalXhKBfz2vuv1BbuARO2slsR3.jnPKW2PIsGErh.j.', '12345675432', 'karachi', 'Male', '11111111111112', NULL);
 
 -- --------------------------------------------------------
 
@@ -291,7 +296,7 @@ CREATE TABLE `seller` (
   `fname` varchar(250) NOT NULL,
   `gender` varchar(250) NOT NULL,
   `email` varchar(250) NOT NULL,
-  `phone` int(11) NOT NULL,
+  `phone` text NOT NULL,
   `city` text NOT NULL,
   `nic` text NOT NULL,
   `password` text NOT NULL
@@ -302,11 +307,10 @@ CREATE TABLE `seller` (
 --
 
 INSERT INTO `seller` (`seller_id`, `fname`, `gender`, `email`, `phone`, `city`, `nic`, `password`) VALUES
-(1, 'pawan', 'Male', 'khatri.pawan22@yahoo.com', 2147483647, '', '', ''),
-(2, 'nothing', 'Male', 'noth@nu.edu.pk', 2147483647, '', '', ''),
-(3, 'zilehuda', 'male', 'zilehuda@gmail.com', 12345, 'karachi', '12345', '$2y$10$BBHB31MjL93SMs1PC1Yo8OU0T67LJAG63noq9G3mf4IUGHxqXQbL.'),
-(4, 'zile', 'Male', 'zile@zile.com', 433434, 'karachi', '34343k', '$2y$10$kUz91W/UHQ3WqS6Xfl3kMOvnUNcI9l8Es5fdUyvW9t99VjyWqIkAu'),
-(5, 'ffkk', 'Male', 'zilehuda@gmail.com', 45454, 'karachi', '45454', '$2y$10$1S5yIs..tUkLXYIxuQbjOO/Kt.cNuj6gtYYlz1Oe4ydZ.jyI2OEM.');
+(1, 'pawan', 'Male', 'khatri.pawan22@yahoo.com', '2147483647', '', '', ''),
+(2, 'nothing', 'Male', 'noth@nu.edu.pk', '2147483647', '', '', ''),
+(3, 'zilehuda', 'male', 'zilehuda@gmail.com', '12345', 'karachi', '12345', '$2y$10$BBHB31MjL93SMs1PC1Yo8OU0T67LJAG63noq9G3mf4IUGHxqXQbL.'),
+(5, 'ffkk', 'Male', 'zilehuda@gmail.com', '45454', 'karachi', '45454', '$2y$10$1S5yIs..tUkLXYIxuQbjOO/Kt.cNuj6gtYYlz1Oe4ydZ.jyI2OEM.');
 
 --
 -- Indexes for dumped tables
@@ -316,7 +320,7 @@ INSERT INTO `seller` (`seller_id`, `fname`, `gender`, `email`, `phone`, `city`, 
 -- Indexes for table `admin`
 --
 ALTER TABLE `admin`
-  ADD PRIMARY KEY (`admin_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `caradd`
@@ -380,7 +384,7 @@ ALTER TABLE `seller`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `caradd`
 --
@@ -390,12 +394,12 @@ ALTER TABLE `caradd`
 -- AUTO_INCREMENT for table `cars`
 --
 ALTER TABLE `cars`
-  MODIFY `car_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `car_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 --
 -- AUTO_INCREMENT for table `client`
 --
 ALTER TABLE `client`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 --
 -- AUTO_INCREMENT for table `hire`
 --
